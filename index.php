1dad94b2dcdb4a0ef3b73bcffe385e3464425fb3
ialwayslookfortime <ialwayslookfortime@gmail.com>
2016-04-11 18:38:55 +0800

    0411-1
    v1

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="bF.css">
+	<meta charset="UTF-8">
+	<title>sadast</title>
+</head>
+<body>
+<div class="wrap">
+	
+
+
+
+<div class="banner">
+	<a class="banner" href="#"><img src="./blueflame/logo.png" alt="baner"></a>
+
+</div>
+
+
+<div class="shop">
+	<a href="#"><img src="./blueflame/menu-1.png" alt="a1"></a>
+	<a href="#"><img src="./blueflame/menu-2.png" alt="a2"></a>
+	<a href="#"><img src="./blueflame/menu-3.png" alt="a3"></a>
+	<a href="#"><img src="./blueflame/menu-4.png" alt="a4"></a>
+	<a href="#"><img src="./blueflame/menu-5.png" alt="a5"></a>
+
+</div>
+
+<div class="asabulo">
+ <h2> Pellentesque dui nibh, porttitor vel, pellentesque dictum, posuere et, nisi. Etiam a orci ut nulla dignissim tempor. Duis porttitor. Nunc vulputate mi nec nisi. Duis ultricies risus sit amet libero. Curabitur et tortor. Praesent tincidunt nisi vel dolor. Praesent faucibus tempor arcu. Pellentesque semper elementum odio. Donec in arcu sit amet sem commodo mattis. Pellentesque condimentum. Suspendisse pede velit, tincidunt sed, vestibulum eu, tempus in, mi. Donec congue ante non tortor. Phasellus ligula augue, porttitor eu, elementum ornare, consequat sed, neque. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Quisque volutpat scelerisque nisl. Aliquam quam. Ut imperdiet nulla at est. Nulla quis nisi non enim ornare mollis. Phasellus fringilla felis sit amet augue.
+</div>
+
+<div class="sofa">
+
+	<a href="#"><img src="./blueflame/demo.png" alt="sofa"></a>
+
+
+</div> 
+
+<div class="product">
+	
+	<div class="product1">
+		<a class="ap1" href="#"><img  src="./blueflame/product-1.png" alt="p1"></a>
+	<div class="tex33"><h3>ssssssssssssssssssssssssss</h3></div>
+	
+	</div>
+	<div class="product1 product2">
+		
+		<a class="ap1" href="#"><img  src="./blueflame/product-2.png" alt="p2"></a>
+	<div class="tex33"><h3>ssssssssssssssssssssssssss</h3></div>
+	</div>
+	
+	<div class="product1 product2">
+		<a class="ap1" href="#"><img  src="./blueflame/product-3.png" alt="p3"></a>
+	<div class="tex33"><h3>ssssssssssssssssssssssssss</h3></div>
+	</div>
+	
+	
+</div>
+
+	
+
+	</div>
+</body>
+</html>
